a58093c28bd52d52f8fb852c59f6404fe5f55d50
Jhonatas Vinicius Neri Dos Santos <ailtonjhonatas@gmail.com>
2024-07-30 12:30:06 -0300

    30/07/2024

--- Exercicios/ex_tad1.c
+++ Exercicios/ex_tad1.c
@@ -10,36 +10,70 @@ typedef struct aluno{
 }Aluno;
 
 void inicializa (int n, Aluno **tab);
+void leAluno (int n, Aluno **tab, int i);
+void imprimeAluno(int n, Aluno** tab, int i);
+void retiraAluno(int n, Aluno** tab, int i);
 
 int main(){
 
   int i;
   Aluno *tab[num];
+
   inicializa(num, tab);
+  leAluno(num, tab, i);
+  imprimeAluno(num, tab, i);
+  printf("Digite a posicão do auno que deseja remover:\n");
+  scanf("%d", &i);
+  retiraAluno(num, tab, i);
 
-  for(i = 0; i < num; i++){
-    if (tab[i] == NULL) {
-      printf("tab[%d] e NULL\n", i);
-    } else {
-      printf("tab[%d] não e NULL\n", i);
-    }
-  }
-
+  i = 0;
   for(i = 0; i < num; i++){
     free(tab[i]);
   }
 
   return 0;
 }
+
 void inicializa (int n, Aluno **tab){
 
   int i;
 
   for(i = 0; i < n; i++){
-    tab[i] = (Aluno *)malloc(sizeof(Aluno));
-  }
-  for(i = 0; i < n; i++){
     tab[i] = NULL;
   }
 
+}
+
+void leAluno (int n, Aluno **tab, int i){
+  for(i = 0; i < n; i++){
+    tab[i] = (Aluno *)malloc(sizeof(Aluno));
+  }
+  for(i = 0; i < n; i++){
+    printf("Digite o nome do aluno:\n");
+    scanf("%s", tab[i]->nome);
+    printf("Digite a matricula do aluno:\n");
+    scanf("%d", &tab[i]->matricula);
+  }
+}
+
+void imprimeAluno(int n, Aluno** tab, int i){
+  for(i = 0; i < n; i++){
+    printf("Aluno: %s\n", tab[i]->nome);
+    printf("Matricula: %d\n", tab[i]->matricula);
+  }
+}
+
+void retiraAluno(int n, Aluno** tab, int i){
+
+  int j;
+  if (i < 0 || i >= n) {
+  printf("Índice inválido.\n");
+  return;
+  }
+  free(tab[i]);
+
+  for(j = i; j < n - 1; j++){
+    tab[j] = tab[j + 1];
+  }
+  tab[n - 1] = NULL;  
 }
