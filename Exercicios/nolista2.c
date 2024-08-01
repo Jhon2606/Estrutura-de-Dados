0f73cc1a32660892146b3e282e3bb2806cdcaca7
Jhonatas Vinicius Neri Dos Santos <94151809+Jhon2606@users.noreply.github.com>
2024-08-01 11:59:25 -0300

    Update nolista2.c

--- Exercicios/nolista2.c
+++ Exercicios/nolista2.c
@@ -12,17 +12,28 @@ void insereElemento(Nolista** l, int v);
 void imprimeelementos(Nolista** l);
 Nolista* buscaElemento(Nolista** l, int v);
 void removeelemento(Nolista** l, int v);
+void liberarLista(Nolista** l);
+void insereOrdenado(Nolista** l, int v);
+int numElementos(Nolista** l);
+Nolista* retpoint(Nolista** l, int v);
 
 int main(){
     Nolista* lista;
     criarLista(&lista);
-    insereElemento(&lista, 1);
-    insereElemento(&lista, 2);
-    insereElemento(&lista, 3);
+    //insereElemento(&lista, 1);
+    //insereElemento(&lista, 2);
+    //insereElemento(&lista, 3);
+    insereOrdenado(&lista, 6);
+    insereOrdenado(&lista, 5);
+    insereOrdenado(&lista, 4);
     imprimeelementos(&lista);
-    buscaElemento(&lista, 2);
-    removeelemento(&lista, 1);
-    imprimeelementos(&lista);
+    //buscaElemento(&lista, 2);
+    //removeelemento(&lista, 1);
+    //imprimeelementos(&lista);
+    //liberarLista(&lista);
+    printf("O numero de elementos na lista e %d\n", numElementos(&lista));
+    Nolista* p = retpoint(&lista, numElementos(&lista));
+    printf("%d\n", p -> info);
 
     return 0;
 }
@@ -96,4 +107,58 @@ void removeelemento(Nolista** l, int v){
     else{
         printf("Lista vazia.\n");
     }
+}
+
+void liberarLista(Nolista** l){
+    Nolista* p, *temp;
+    for(p = *l;p != NULL;p = temp){
+    temp = p-> prox;
+    free(p);
+    }
+    *l = NULL;
+}
+
+void insereOrdenado(Nolista** l, int v){
+    Nolista* p, *ant = NULL;
+    Nolista* novo = (Nolista*)malloc(sizeof(Nolista));
+    if(novo != NULL){
+        novo -> info = v;
+        for(p = *l; p != NULL && p -> info < v; p = p -> prox)
+        ant = p;
+    novo -> prox = p;
+    if(ant == NULL){
+        *l = novo;
+
+    }
+    else{
+        ant -> prox = novo;
+    }
+}
+else{
+    printf("Nao foi possivel alocar espaco.\n");
+}
+}
+
+int numElementos(Nolista** l){
+
+    int i = 0;
+    Nolista* p;
+    for(p = *l; p!= NULL; p = p -> prox){
+        i++;
+    }
+    return i;
+}
+
+Nolista* retpoint(Nolista** l, int v){
+
+    Nolista* p;
+    int count = 0;
+
+    for(p = *l; p!= NULL; p = p -> prox){
+        count++;
+        if(count == v){
+            return p;
+        }
+    }
+    return NULL;
 }
