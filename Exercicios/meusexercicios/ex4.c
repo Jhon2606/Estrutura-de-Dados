743e2084667284a74f488c486b85160567338fb3
Jhonatas Vinicius Neri Dos Santos <94151809+Jhon2606@users.noreply.github.com>
2024-09-02 14:39:31 -0300

    Update ex4.c

--- Exercicios/meusexercicios/ex4.c
+++ Exercicios/meusexercicios/ex4.c
@@ -1,86 +1,90 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
 typedef struct nolista{
-  float info;
-  struct nolista* prox;
+    float info;
+    struct nolista* prox;
 }Nolista;
 
 typedef struct pilha{
-  struct nolista* prim;
+    struct nolista* prim;
 }Pilha;
 
 Pilha* CriarPilha(){
-  Pilha* p = (Pilha*)malloc(sizeof(Pilha));
-  if(p != NULL){
+    Pilha* p = (Pilha*)malloc(sizeof(Pilha));
     p->prim = NULL;
     return p;
-  }
 }
 
 int EstaVazia(Pilha* p){
-  return(p->prim == NULL);
-}
-
-void Linha(){
-    printf("\n--------------------\n");
+    return(p->prim == NULL);
 }
 
 void Push(Pilha* p, float v){
-  Nolista* novo = (Nolista*)malloc(sizeof(Nolista));
-  if(novo!=NULL){
+Nolista* novo = (Nolista*)malloc(sizeof(Nolista));
+if(novo!=NULL){
     novo->info = v;
     novo->prox = p->prim;
     p->prim = novo;
-  }else{
-    printf("Espaco nao alocado.\n");
-  }
+}
+else{
+    printf("Espaco nao alocao.\n");
+}
 }
 
-void imprimePilha(Pilha* p){
-  Nolista* i;
-  if(!EstaVazia(p)){
-    for(i = p->prim; p != NULL; i = i->prox){
-      printf("[%.1f]", i->info);
+//A funcao de impressao e so para teste, normalmente nao usamos a impressao na pilha
+
+void ImprimePilha(Pilha* p){
+    Nolista* i;
+    if(!EstaVazia(p)){
+        for(i = p->prim; i != NULL; i = i->prox){
+            printf("[%.1f] ", i->info);
+        }
     }
-  }else{
-    printf("Lista vazia.\n");
-  }
+    else{
+        printf("A lista esta vazia.\n");
+    }
 }
 
 float Pop(Pilha* p){
-  Nolista* t;
-  float v;
-  if(!EstaVazia(p)){
-    t = p->prim;
-    p->prim = t->prox;
-    v = t->info;
-    free(t);
-    return v;
-  }else{
-    printf("A lista esta vazia.\n");
-  }
+    Nolista* t;
+    float v;
+    if(!EstaVazia(p)){
+        t = p->prim;
+        p->prim = t->prox;
+        v = t->info;
+        free(t);
+        return v;
+    }
+    else{
+        printf("A lista esta vazia.\n");
+    }
 }
 
-void LiberarPilha(Pilha* p){
-  Nolista* i, *temp;
-  if(!EstaVazia(p)){
-    for(i = p->prim; i != NULL; i = temp){
-    temp = i->prox;
-    free(i);
+void Liberarpilha(Pilha* p){
+    Nolista* t, *temp;
+    if(!EstaVazia(p)){ 
+        for(t = p->prim; p != NULL; t = temp){
+            temp = t->prox;
+            free(t);
+        }
     }
-  free(p);
-  }
+    else{
+        printf("A lista esta vazia.\n");
+    }
+    free(p);
 }
 
 float VerTopo(Pilha* p){
-  if(!EstaVazia(p)){
-    return p->prim->info;
-  }else{
-    printf("A lista esta vazia.\n");
-  }
+    float a;
+    Nolista* t;
+    if(!EstaVazia(p)){
+        return p->prim->info;
+    }
+    else{
+        printf("A lista esta vazia.\n");
+    }
 }
-
 void ConcatenaPilhas(Pilha* p1, Pilha* p2){
     Pilha* i, *temp = CriarPilha();
     while(!EstaVazia(p2)){
@@ -122,18 +126,18 @@ void ConcatenaSemTAD(Pilha* p1, Pilha* p2) {
 int main(){
     Pilha* pilha = CriarPilha();
     Pilha* pilha2 = CriarPilha();
-    Linha();
     Push(pilha, 1);
     Push(pilha, 2);
     Push(pilha, 3);
     ImprimePilha(pilha);
-    Linha();
+    printf("\n");
     Push(pilha2, 4);
     Push(pilha2, 5);
     Push(pilha2, 6);
     ImprimePilha(pilha2);
-    Linha();
+    printf("\n");
     ConcatenaSemTAD(pilha, pilha2);
     ImprimePilha(pilha);
+    printf("\n");
   return 0;
 }
